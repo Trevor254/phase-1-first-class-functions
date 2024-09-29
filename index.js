9c5dfb3a8a182ccf906dc6b6c06d0cf7e8fbab52
trevor <trevoromondi607@gmail.com>
2024-09-29 16:57:57 +0300

    function practice

---
+++ index.js
@@ -0,0 +1,18 @@
+function receivesAFunction(callback){
+  callback()
+}
+
+function returnsANamedFunction(){
+   return function dillinjer(){}
+}
+
+function returnsAnAnonymousFunction(){
+   return function(){
+
+   }
+   
+}
+
+
+
+
